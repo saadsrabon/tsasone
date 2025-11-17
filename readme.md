6dbca469f9825cb4642c76b1005daeda2328271b
Saad Srabon <saadsrabon2@gmail.com>
2025-11-17 17:54:19 +0600

    first commit

---
+++ readme.md
@@ -0,0 +1,53 @@
+
+What are some differences between interfaces and types in TypeScript? 
+Interface
+নিম্নে টাইপ আর ইন্টারফেস এর মধ্যে পার্থক্য আলোচনা করা হলঃ
+
+অবজেক্ট বা ক্লাসের স্ট্রাকচার/কনট্র্যাক্ট ডিফাইন করতে ব্যবহৃত।
+
+extends দিয়ে এক্সটেন্ড করা যায়।
+
+একাধিক ইন্টারফেস একই নামে ডিক্লেয়ার করলে মার্জ হয় (Declaration merging)।
+
+মূলত অবজেক্ট ও ক্লাসের কনট্র্যাক্টের জন্য।
+
+Type Alias
+
+অবজেক্ট, ইউনিয়ন, ইন্টারসেকশন, টুপল, প্রিমিটিভ সবকিছুর টাইপ ডিফাইন করা যায়।
+
+& ব্যবহার করে একাধিক টাইপ মিলানো যায়।
+
+ডিক্লেয়ারেশন মার্জিং সম্ভব নয়।
+
+ফ্লেক্সিবল টাইপ কম্পোজিশন বা ইউনিয়ন/টুপলের জন্য।
+
+
+<!--  -->
+What is the use of the keyof keyword in TypeScript? Provide an example.
+TypeScript-এ keyof কীওয়ার্ড
+
+অবজেক্টের সব key বের করা।
+
+টাইপ-সেফ প্রপার্টি এক্সেস।
+
+জেনেরিক ফাংশন/ক্লাসে টাইপ-সেফ ডাইনামিক প্রপার্টি।
+
+কাস্টম টাইপ ইউটিলিটি তৈরিতে ব্যবহার।
+
+type User = {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+};
+
+
+
+type UserKeys = keyof User; 
+
+
+function getProperty(obj: User, key: keyof User) {
+  return obj[key];
+}
+
+const user: User = { name: "Saad", age: 28, isAdmin: true };
+console.log(getProperty(user, "name")); 
---
+++ solution.ts
@@ -0,0 +1,115 @@
+type formatValueType = string | number | boolean;
+function formatValue(value: formatValueType): formatValueType {
+  if (typeof value === 'string') {
+    return value.toUpperCase();
+  } else if (typeof value === 'number') {
+    return value * 10;
+  } else {
+    return !value;
+  }
+}
+
+type getLengthData = any[] | string;
+function getLength(value: getLengthData): number {
+  if (typeof value === 'string') {
+    return value.length;
+  } else if (Array.isArray(value)) {
+    return value.length;
+  }
+  return 0;
+}
+
+class Person {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  getDetails(): string {
+    return `'Name: ${this.name}, Age: ${this.age}'`;
+  }
+}
+
+type filtersRating = {
+  title: string;
+  rating:  0 | 1 | 2 | 3 | 4 | 5;
+};
+function filterByRating<T extends filtersRating>(data: T[]): T[] {
+  const filteredRatingData = data.filter((item) => item.rating >= 4);
+  return filteredRatingData;
+}
+
+type userData = {
+  name: string;
+  id: number;
+  email: string;
+  isActive: boolean;
+};
+function filterActiveUsers<T extends userData>(users: T[]): T[] {
+  return users.filter((user) => user.isActive === true);
+}
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+function printBookDetails(bookData: Book): string {
+  let result = `Title: ${bookData?.title}, Author: ${bookData?.author}, Published: ${
+    bookData?.publishedYear
+  }, Available:  ${bookData.isAvailable ? 'Yes' : 'No'}`;
+  return result;
+}
+
+
+
+function getUniqueValues(arr: (number | string)[], arr2: (number | string)[]): (number | string)[] {
+  let temp: Record<string, number | string> = {};
+  let result: (number | string)[] = [];
+  arr.forEach((element: number | string) => {
+    if (temp[element] === undefined) {
+      temp[element] = element;
+      result.push(element);
+    } else {
+      return;
+    }
+  });
+
+  arr2.forEach((element) => {
+    if (temp[element] === undefined) {
+      temp[element] = element;
+      result.push(element);
+    } else {
+      return;
+    }
+  });
+
+  return result;
+}
+
+interface product {
+  name: string;
+  price: number;
+  quantity: number;
+  discount?: number;
+}
+function calculateTotalPrice(arr: product[]): number {
+  let total: number = 0;
+  arr.forEach((element) => {
+    let itemTotal: number = element.price * element.quantity;
+    
+    const discount = element.discount ?? 0;
+
+    let discountedItem: number = itemTotal * (1 - discount / 100);
+
+    total += discountedItem;
+  });
+  return total;
+}
+
+
+
+
